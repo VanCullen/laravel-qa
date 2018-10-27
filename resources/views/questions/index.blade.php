704140533261d2726a783207213085ed1a01ec72
Yahya <yahyatajany@gmail.com>
2018-10-27 12:36:02 +0100

    Authorizing using Gates

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -33,12 +33,17 @@
                                     <div class="d-flex align-items-center">
                                         <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                         <div class="ml-auto ">
+                                            @if(Auth::user()->can('update-question',$question))
                                             <a href="{{ route('questions.edit' , $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            @endif
+
+                                            @if(Auth::user()->can('delete-question',$question))
                                             <form class="form-delete" action="{{ route('questions.destroy',$question->id) }}" method="post">
                                                 @method('DELETE')
                                                 @csrf
                                                 <input type="submit" value="Delete" class="btn btn-sm btn-outline-danger" onclick="return confirm('You\'re about to delete this question ?')">
                                             </form>
+                                            @endif
                                         </div>
                                     </div>
                                     <p class="lead">Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
