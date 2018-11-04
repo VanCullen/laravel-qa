48958c304ea0cb79b51595a943e2e9b9d467c18f
Yahya <yahyatajany@gmail.com>
2018-11-04 13:25:36 +0000

    Update the best answer id when its removed

--- app/Answer.php
+++ app/Answer.php
@@ -17,7 +17,13 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = null;
+                $question->save();
+            }
         });
     }
     public function question(){
@@ -38,4 +44,7 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted':'';
+}
 }
